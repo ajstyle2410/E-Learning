8115040e3de3ed6b7ee04963c3f8c82932be7227
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2024-06-06 18:20:42 +0530

    Create the Elearning Project Structure

---
+++ Elearnings/src/main/java/com/elearning/ElearningsApplication.java
@@ -0,0 +1,14 @@
+package com.elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningsApplication {
+
+	public static void main(String[] args) {
+	
+		SpringApplication.run(ElearningsApplication.class, args);
+	}
+
+}
