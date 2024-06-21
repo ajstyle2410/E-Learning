620234b2b90e78c7b77603e410672c5a49eeaede
ajstyle <ajdeshmukh241@gmail.com>
2024-06-21 10:17:36 +0530

    Create InterviewQuestionServiceInterface.java

---
+++ Elearnings/src/main/java/com/elearning/service/InterviewQuestionServiceInterface.java
@@ -0,0 +1,10 @@
+package com.elearning.service;
+
+
+import com.elearning.dto.InterviewQuestionDTO;
+
+public interface InterviewQuestionServiceInterface {
+
+	 public String addInterviewQuestion(InterviewQuestionDTO dto);
+
+}
