76eec50a34ba78d221596ba641a7cefba0e70439
ajstyle <ajdeshmukh241@gmail.com>
2024-06-28 17:54:41 +0530

    Update viewInterview.js

--- Elearnings/src/main/resources/static/js/Interview/view/viewInterview.js
+++ Elearnings/src/main/resources/static/js/Interview/view/viewInterview.js
@@ -1,22 +1,47 @@
 
-function viewUploadCourseId()
-{
+function viewUploadCourseId() {
 	var courseId = document.getElementById("courseId").value;
-	alert(courseId)
-	     var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("SelectChapter").innerHTML = this.responseText;
-                }
-            };
-          
-                xhttp.open("POST", '/sendUploadCourseId/' + courseId, true);
-        
-            xhttp.send();
-	
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("SelectChapter").innerHTML = this.responseText;
+		}
+	};
+
+	xhttp.open("POST", '/sendUploadCourseId/' + courseId, true);
+
+	xhttp.send();
+
 }
 
-function viewUploadChapterId()
-{
-	
+function viewUploadChapterId() {
+
+	var chapterId = document.getElementById("chapterId").value;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("Topics").innerHTML = this.responseText;
+		}
+	};
+
+	xhttp.open("POST", '/uploadChapterWiseTopicQuestions/' + chapterId, true);
+
+	xhttp.send();
+
 }
+
+function viewTopic(topicId) 
+{
+	var http = new XMLHttpRequest();
+	alert("Topic Id " + topicId)
+	http.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+
+			alert(this.status)
+			document.getElementById("InterviewQuestion").innerHTML = this.responseText;
+		}
+	};
+	http.open("POST", '/uploadTopicId/' + topicId, true);
+	http.send();
+}
+
