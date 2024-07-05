0e8cc56bccae795910913cde1c03f7d6eb532073
ajstyle <ajdeshmukh241@gmail.com>
2024-07-05 14:53:41 +0530

    Update UserProfileController.java

--- Elearnings/src/main/java/com/elearning/controller/UserProfileController.java
+++ Elearnings/src/main/java/com/elearning/controller/UserProfileController.java
@@ -1,16 +1,38 @@
 package com.elearning.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.elearning.model.Course;
+import com.elearning.service.CourseServiceInterface;
+
 @RestController
 public class UserProfileController {
 
+	@Autowired
+	private CourseServiceInterface courseServiceInterface;
+	
+	
 	@GetMapping("user")
 	 public ModelAndView userProfile()
 	 {
 		 return new ModelAndView("./user/UserDashboard");
+	 }
+	
+	@GetMapping("/")
+	 public ModelAndView  viewHomePage(ModelMap map)
+	 {
+		List<Course>  list = courseServiceInterface.viewCourse();
+		
+		map.addAttribute("list", list);
+		
+		
+		return new ModelAndView("HomePage");
 	 }
 	
 }
