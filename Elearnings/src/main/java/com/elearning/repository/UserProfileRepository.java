549b129edcaf50aeef6035735bdb6399c7cf232b
ajstyle <ajdeshmukh241@gmail.com>
2024-07-27 13:40:12 +0530

    Update UserProfileRepository.java

--- Elearnings/src/main/java/com/elearning/repository/UserProfileRepository.java
+++ Elearnings/src/main/java/com/elearning/repository/UserProfileRepository.java
@@ -1,9 +1,14 @@
 package com.elearning.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.elearning.model.UserProfile;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+     
+	@Query("select e.Email from UserProfile e where e.Email=:Email ")
+	public UserProfile findByEmail(@Param("Email")String Email);
 
 }
